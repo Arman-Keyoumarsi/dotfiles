c92ce1ac2874efd80859044efa3625a686c94727
Arman Keyoumarsi <arman@keyoumarsi.com>
2020-06-20 12:00:51 +0100

    Updated Bootstrap Script

--- ub18lts-bootstrap.sh
+++ ub18lts-bootstrap.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+#Removing tiny vim
+sudo apt remove --assume-yes vim-tiny
 sudo apt-get update
 
 # Installing essentialse
-sudo apt-get install -y build-essential vim curl wget python3-dev ripgrep cmake tree zip
+sudo apt-get install -y build-essential
+sudo apt-get install -y vim 
+sudo apt-get install -y curl 
+sudo apt-get install -y wget 
+sudo apt-get install -y python3-dev 
+sudo apt-get install -y ripgrep 
+sudo apt-get install -y tree 
+sudo apt-get install -y zip
 sudo apt-get install -y tmux
 sudo apt-get install -y mosh
 
